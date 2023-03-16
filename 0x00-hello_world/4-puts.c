b6de3959eb00779722552b825e4989b4c5fe64e1
Seeletsa <seeletsagrey@gmail.com>
2023-03-16 05:48:25 -0700

    TASK4 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stio.h>
+/**
+ * main - An entry point for a programme that use the puts function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
