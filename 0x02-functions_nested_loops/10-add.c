09d85b7a5d737e501a7d6beca4e7769ce63fe489
Seeletsa <seeletsagrey@gmail.com>
2023-03-21 14:31:27 -0700

    Task 10 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add -  function that adds two integers and returns the result
+ * @n: first parameter
+ * @m: second parameter
+ * Return: value
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
