de7167dea179251e208fa68bebcb4fbe11990733
Seeletsa <seeletsagrey@gmail.com>
2023-03-21 17:45:08 -0700

    Task 12 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_times_table - function that prints the n times table
+ * @n: parameter
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int m;
+	int p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0 ; i <= n ; i++)
+		{
+			for (m = 0 ; m <= n ; m++)
+			{
+				p = m * i;
+				if (m == 0)
+				{
+					_putchar(p + '0');
+				}
+				else if (p < 10 && m != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+				else if (p >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
