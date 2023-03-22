1c7268ee30c777e40a4e384cb3a37ff41aebdb52
Seeletsa <seeletsagrey@gmail.com>
2023-03-21 18:24:14 -0700

    Task 12 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,20 +20,26 @@ void print_times_table(int n)
 				if (m == 0)
 				{
 					_putchar(p + '0');
-				}
-				else if (p < 10 && m != 0)
+				} else if (p < 10 && m != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(p + '0');
-				}
-				else if (p >= 10)
+				} else if (p >= 10 && p < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				} else if (p >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10) % 10) + '0');
 					_putchar((p % 10) + '0');
 				}
 			}
@@ -41,4 +47,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
