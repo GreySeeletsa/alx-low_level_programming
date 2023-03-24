f4db4808bffd0610d730bd6704ddc16d17c45cc3
Seeletsa <seeletsagrey@gmail.com>
2023-03-24 09:51:58 -0700

    Task 4 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers -  function that prints the numbers but 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (!(a == '2' || a == '4'))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
