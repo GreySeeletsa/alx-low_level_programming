7391126f2668dcc874ff9faeddf2bdfa11da4e74
Seeletsa <seeletsagrey@gmail.com>
2023-03-24 12:08:25 -0700

    Task 9 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+/**
+ * main - check the code
+ * print_square - declaration
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
