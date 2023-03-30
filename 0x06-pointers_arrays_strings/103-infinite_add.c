df90bdedcf0667016cf10777e98f3c106ea49230
Seeletsa <seeletsagrey@gmail.com>
2023-03-30 16:17:07 -0700

    Task 11 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,30 +1,5 @@
 #include "main.h"
 /**
- * rev_string - array
- * @n: parameter
- * Return: 0
- */
-
-void rev_string(char *n)
-{
-	int a = 0;
-	int b = 0;
-	char temp;
-
-	while (*(n + a) != '\0')
-	{
-		a++;
-	}
-	a--;
-
-	for (b = 0; b < a; b++, a--)
-	{
-		temp = *(n + b);
-		*(n + b) = *(n + a);
-		*(n + a) = temp;
-	}
-}
-/**
  * infinite_add - function that adds two numbers
  * @n1: value
  * @n2: value
@@ -35,42 +10,44 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, a = 0, b = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + a) != '\0')
-		a++;
-	while (*(n2 + b) != '\0')
-		b++;
-	a++;
-	b++;
-	if (b >= size_r || a >= size_r)
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	while (b >= 0 || a >= 0 || overflow == 1)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		if (a < 0)
-			val1 = 0;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			val1 = *(n1 + a) - '0';
-		if (b < 0)
-			val2 = 0;
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
 		else
-			val2 = *(n2 + b) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		b++;
-		a++;
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
