903d9b23b59389080416a71ddbbf8428d4c6571d
Seeletsa <seeletsagrey@gmail.com>
2023-03-30 16:26:32 -0700

    Task 3 0x06 pointers-arrays strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int a;
 
-	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
 	{
 		if (s1[a] != s2[a])
 		{
-			if (s1[a] < s2[a])
-				return (s1[a] - s2[a]);
-			else if (s1[a] > s2[a])
-				return (s1[a] - s2[a]);
+			return (s1[a] - s2[a]);
 		}
-		else
-			return (0);
+		a++;
 	}
 	return (0);
 }
