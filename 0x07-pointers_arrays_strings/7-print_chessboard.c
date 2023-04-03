f1407f5acf7afde671cb87206085cf1c277065be
Seeletsa <seeletsagrey@gmail.com>
2023-04-03 11:53:44 -0700

    Task 6 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: parameter
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 0; n++)
+	{
+		for (m = 0; m < 0; m++)
+			_putchar(a[n][m]);
+		_putchar('\0');
+	}
+}
