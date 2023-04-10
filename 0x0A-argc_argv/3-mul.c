732211a42ca935678001674166e3608d72f808fe
Seeletsa <seeletsagrey@gmail.com>
2023-04-10 06:55:58 -0700

    Task 3 0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - from string to integer
+ * @s: string
+ * Return: conveted string
+ */
+
+int _atoi(char *s)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	e = 0;
+	f = 0;
+
+	while (s[d] != '\0')
+		d++;
+	while (a < d && e == 0)
+	{
+		if (s[a] == '-')
+			++b;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			f = s[a] - '0';
+
+			if (b % 2)
+				f = f;
+
+			c = c * 10 + f;
+
+			e = 1;
+
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
+		}
+	a++;
+	}
+
+	if (e == 0)
+		return (0);
+	return (c);
+}
+/**
+ * main -  program that multiplies two numbers
+ * @argv: arguments vector
+ * @argc: counts arguments
+ * Return: 0 if no error otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int m;
+	int n1;
+	int n2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	n1 = _atoi(argv[1]);
+
+	n2 = _atoi(argv[2]);
+
+	m = n1 * n2;
+
+	printf("%d\n", m);
+
+	return (0);
+}
