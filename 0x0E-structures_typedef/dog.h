3c67b760123b598a4876a85894deec37c8d9f00e
Seeletsa <seeletsagrey@gmail.com>
2023-04-17 13:59:02 -0700

    Task 0 dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,17 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * dog_t - used for struct dog
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog_t;
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
