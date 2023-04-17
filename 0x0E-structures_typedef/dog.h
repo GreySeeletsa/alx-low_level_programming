4847dfc7d2b735a30948320b1d09217cfb4eb7e7
Seeletsa <seeletsagrey@gmail.com>
2023-04-17 13:52:27 -0700

    Task 1 dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,14 @@ struct dog
 /**
  * dog_t - used for struct dog
  */
-typedef struct dog dog_type;
+typedef struct dog dog_t;
 
-void init_dog(struct dog *strdog, char *name, float age, char *owner);
-void print_dog(struct dog *strdog);
-dog_type *diff_dog(char *name, float age, char *owner);
-void free_dog(dog_type *strdog);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *str);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
